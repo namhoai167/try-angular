7f67dbac18a854bf52846cc10a15ea88064db63f
namvhnguyen <namhoai167@gmail.com>
2022-11-17 16:19:16 +0700

    Update the ProductService
    Update with getProducts() return Observable
    Inject MessageService into contructor()
    Send a message from ProductService
    https://angular.io/tutorial/toh-pt4

--- try-angular/src/app/product.service.ts
+++ try-angular/src/app/product.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+
+import { Product } from './product';
 import { MessageService } from './message.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
 
   constructor(private messageService: MessageService) { }
+
+  getProducts(): Observable<Product[]> {
+    const products = of([]);
+    this.messageService.add('ProductService: fetched products');
+    return products;
+  }
+
 }

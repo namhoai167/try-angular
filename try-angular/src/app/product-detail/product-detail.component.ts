56eea880ac002fbdf8fb94a3d9d6bdd0fd429b16
namvhnguyen <namhoai167@gmail.com>
2022-11-17 17:17:51 +0700

    Extract the id route parameter, add a go back button, add the private CSS styles
    https://angular.io/tutorial/toh-pt5#herodetailcomponent

--- try-angular/src/app/product-detail/product-detail.component.ts
+++ try-angular/src/app/product-detail/product-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
+import { ProductService } from '../product.service';
 import { Product } from '../product';
 @Component({
   selector: 'app-product-detail',
@@ -10,9 +13,22 @@ export class ProductDetailComponent implements OnInit {
 
   @Input() product?: Product;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
+    this.getProduct();
   }
 
+  getProduct(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.productService.getProduct(id).subscribe(product => this.product = product);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }

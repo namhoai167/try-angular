3eb3e57fb815a87008f173d8ebb045bb364ae0ca
namvhnguyen <namhoai167@gmail.com>
2022-11-18 08:57:11 +0700

    Add and import HTTP service
    https://angular.io/tutorial/toh-pt6#enable-http-services
    https://angular.io/tutorial/toh-pt6#heroes-and-http

--- try-angular/src/app/product.service.ts
+++ try-angular/src/app/product.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
 
@@ -10,7 +11,12 @@ import { MessageService } from './message.service';
 })
 export class ProductService {
 
-  constructor(private messageService: MessageService) { }
+  private productsUrl = 'http://127.0.0.1:8000/api/products/'
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
 
   getProducts(): Observable<Product[]> {
     const products = of([]);

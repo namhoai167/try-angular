e742ec23fe4cdcbc466945bd6278d1377dd97fa0
namvhnguyen <namhoai167@gmail.com>
2022-11-18 09:44:20 +0700

    Add the missing FormsModule for NgModel

--- try-angular/src/app/app.module.ts
+++ try-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
   ],
